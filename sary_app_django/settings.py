b5eba9b23c5c8d8310d41cf5031f497d70a4df57
Abdulrahman Alanazi <iabdulrahman.91@gmail.com>
2022-02-12 00:32:54 +0300

    pass SECRET_KEY as env var during runtime

--- sary_app_django/settings.py
+++ sary_app_django/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-uzqo*hxktxtrb85fw8(cc4&p4x$z5c@b0s1+0j-)s%e8vtku17'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
