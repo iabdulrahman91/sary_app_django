40cac2c27dbb4c6964a99ed89c8781a5d88ed144
Abdulrahman Alanazi <iabdulrahman.91@gmail.com>
2022-02-12 23:44:34 +0300

    update prod setting

--- sary_app_django/settings_prod.py
+++ sary_app_django/settings_prod.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
