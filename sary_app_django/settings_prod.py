00b5b38624d827ccf3555c606d25385b571f9e38
Abdulrahman Alanazi <iabdulrahman.91@gmail.com>
2022-02-13 12:15:06 +0300

    allow all host

--- sary_app_django/settings_prod.py
+++ sary_app_django/settings_prod.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
